768fd0fa604b6bb3bcb6df66d4d9d2718985517c
alikow216 <64177595+alikow216@users.noreply.github.com>
2020-04-28 06:07:15 +0300

    java dilinde merhaba dunya
    aliyow tarafaından kodlanmıştır

---
+++ Merhabadünya.java
@@ -0,0 +1,10 @@
+public static main(Stiring[] args {
+public class MERHABA DUNYA 
+int x=10; 
+int ali;
+sadi = 10;  
+System.out.println("Merhaba Dünya"+ ali); 
+ali = 20; 
+System.out.println("Merhaba Dünya"+ ali);
+Stiring s = "benim ismim ali yıldırım  ";
+System.out.println(" dizgi"+ s+ "pi":"+pi);
